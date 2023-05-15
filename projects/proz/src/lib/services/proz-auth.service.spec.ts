465581d004e905f4b1d5897333992b403640b6c9
benedict <berjistechnologies@gmail.com>
2023-05-15 15:01:41 +0300

    Add ProzTestApp

--- projects/proz/src/lib/services/proz-auth.service.spec.ts
+++ projects/proz/src/lib/services/proz-auth.service.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ProzAuthService } from './proz-auth.service';
 
 describe('ProzAuthService', () => {
   let service: ProzAuthService;
+  let httpMock: HttpTestingController; // declare httpMock here
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Import HttpClientTestingModule here
+      providers: [ProzAuthService]
+    });
     service = TestBed.inject(ProzAuthService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
