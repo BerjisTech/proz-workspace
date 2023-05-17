35b90a4393397a65e9dafc1cfaccabfd493b6260
benedict <berjistechnologies@gmail.com>
2023-05-17 14:09:02 +0300

    Add User action buttons in test app and related library functions

--- projects/proz-test-app/src/app/pages/landing/landing.component.ts
+++ projects/proz-test-app/src/app/pages/landing/landing.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { ProfileService } from 'projects/proz/src/lib/services/profile.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent {
+  constructor(private profileService: ProfileService) { }
+
+  getActiveUser = () => {
+    this.profileService.getActiveUser().subscribe(
+      response => {
+        alert('Get user successful')
+        console.log(response)
+      },
+      error => {
+        alert('Get user failed')
+        console.log(error)
+      }
+    )
+  }
+
+  getUsers = () => {
+    this.profileService.getUsers().subscribe(
+      response => {
+        alert('Get users successful')
+        console.log(response)
+      },
+      error => {
+        alert('Get users failed')
+        console.log(error)
+      }
+    )
+  }
+
+  getUser = () => {
+    const userUuid = 'b8d6f0d0-0c2a-11eb-9c6e-0242ac130002'
+    this.profileService.getUser(userUuid).subscribe(
+      response => {
+        alert('Get user successful')
+        console.log(response)
+      },
+      error => {
+        alert('Get user failed')
+        console.log(error)
+      }
+    )
+  }
 
 }
--- projects/proz/src/lib/services/profile.service.ts
+++ projects/proz/src/lib/services/profile.service.ts
@@ -10,19 +10,29 @@ export class ProfileService {
 
   constructor(private http: HttpClient) { }
 
+  getActiveUser(): Observable<any> { // Get a User object for the currently authenticated user.
+    return this.http.get(`${environment.apiUrl}user`);
+  }
+  getUsers(): Observable<any> { // Get a collection of UserSummary records from a comma-separated list of UUIDs.
+    return this.http.get(`${environment.apiUrl}users`);
+  }
+  getUser(userUuid: String): Observable<any> { // Get a single UserSummary record for a specified UUID.
+    return this.http.get(`${environment.apiUrl}users/${userUuid}`);
+  }
+
   // To get user data
   getUserData(userId: string): Observable<any> {
-    return this.http.get(`${environment.apiUrl}api/user/${userId}`);
+    return this.http.get(`${environment.apiUrl}user/${userId}`);
   }
 
   // To update user data
   updateUser(userId: string, updatedData: any): Observable<any> {
-    return this.http.put(`${environment.apiUrl}api/user/${userId}`, updatedData);
+    return this.http.put(`${environment.apiUrl}user/${userId}`, updatedData);
   }
 
   // To delete profile image
   deleteProfileImage(userId: string): Observable<any> {
-    return this.http.delete(`${environment.apiUrl}api/user/${userId}/image`);
+    return this.http.delete(`${environment.apiUrl}user/${userId}/image`);
   }
 
 }
