57210ae7ce8c8fd2fd1e49aac827da814b4db8c9
benedict <berjistechnologies@gmail.com>
2023-05-18 12:12:47 +0300

    CPN and Plus badges

--- projects/proz/src/lib/services/badges.service.ts
+++ projects/proz/src/lib/services/badges.service.ts
@@ -6,4 +6,13 @@ import { Injectable } from '@angular/core';
 export class BadgesService {
 
   constructor() { }
+
+  getPlusMembershipBadge = (plusMembership: string) => {
+    return plusMembership && 'https://sslcdn.proz.com/zf/images/professional-membership/plus_badge.png'
+  }
+
+  getCertificationBadge = (certification: string) => {
+    return certification && 'https://sslcdn.proz.com/zf/images/proz-nliv-homepage/home-cpn.png'
+  }
+
 }
