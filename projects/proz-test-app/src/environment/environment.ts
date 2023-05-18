ec9ba454a82e52f384ff449210452c096a0cb4e9
benedict <berjistechnologies@gmail.com>
2023-05-18 12:55:13 +0300

    Update token with availability scope

--- projects/proz-test-app/src/environment/environment.ts
+++ projects/proz-test-app/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    prozToken: '2f1610d635cd3e7461c1456ab5ddda20a67bbdc9',
+    prozToken: 'ea2b121282fa4f74749ab5440934b1e0f888ccce',
     testUuid: '3fe4056c-a55e-4d8a-9f86-d2d9eb2bd78a',
     proz_client_id: 'f45664cfa9e1a2b6a8e74f731b954fff13624fce',
     redirect_uri: 'http://localhost:4200/',
