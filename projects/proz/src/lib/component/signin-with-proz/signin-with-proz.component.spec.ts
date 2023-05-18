cc66c6c054deb5f9d05594bb36875d7d203fc3ae
benedict <berjistechnologies@gmail.com>
2023-05-18 09:53:12 +0300

    Fix http error on sign in with prtoz spec file

--- projects/proz/src/lib/component/signin-with-proz/signin-with-proz.component.spec.ts
+++ projects/proz/src/lib/component/signin-with-proz/signin-with-proz.component.spec.ts
@@ -1,16 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { SigninWithProzComponent } from './signin-with-proz.component';
 
 describe('SigninWithProzComponent', () => {
   let component: SigninWithProzComponent;
   let fixture: ComponentFixture<SigninWithProzComponent>;
+  let httpMock: HttpTestingController; // declare httpMock here
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SigninWithProzComponent]
+      declarations: [SigninWithProzComponent],
+      imports: [HttpClientTestingModule], // Import HttpClientTestingModule here
     });
     fixture = TestBed.createComponent(SigninWithProzComponent);
+    httpMock = TestBed.inject(HttpTestingController);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
